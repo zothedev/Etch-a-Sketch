9a20eb00c23d175cb694c510998522b0da8ca832
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-17 16:43:25 -0500

    Adjust grid-creating functions to modular

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let container = document.querySelector("div");
 createGrid();
 
 // creating the 16x16 square grid
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
-        create16Squares();
+function createGrid(rows = 16) {
+    for (let i = 0; i < rows; i++) {
+        createSquares(rows);
     }
 }
 
-// create one row of 16 squares
-function create16Squares() {
+// create one row of  squares
+function createSquares(squares = 16) {
     let rowContainer = document.createElement("div");
     rowContainer.classList.add("rowContainer")
     container.appendChild(rowContainer);
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < squares; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         rowContainer.appendChild(square);
