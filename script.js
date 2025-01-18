0a96fe6e9d29f67da5babc780289d6260c2e9538
zothedev <64105313+zothedev@users.noreply.github.com>
2025-01-18 13:33:43 -0500

    Modify change grid function to take up same space
    The change grid button now shrinks or enlarges
    the squares to take up the same amount of space
    every time. This amount of space is equal to a
    16x16 grid, or 768px x 768px.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // current status:
-// - change grid button disappears on-click
 // - new grid works, but needs to be stretched to fill the space that a 16x16 grid takes up
+// 768 x 768
 
 let container = document.querySelector(".gridContainer");
 createGrid();
@@ -18,9 +18,13 @@ function createSquares(squares = 16) {
     rowContainer.classList.add("rowContainer")
     container.appendChild(rowContainer);
 
+    let squareSize = 768 / squares;
+
     for (let i = 0; i < squares; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = squareSize + 'px';
+        square.style.height = squareSize + 'px';
         rowContainer.appendChild(square);
     }
 }
